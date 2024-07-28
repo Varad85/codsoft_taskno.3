154db41f0be8aa331d9edc13a7b8ceceed1bdf82
Varad Kanekar <kanekarvarad@gmail.com>
2024-07-28 20:13:15 +0530

    method to read balance from balance file

--- BankAccount.java
+++ BankAccount.java
@@ -26,4 +26,12 @@ class BankAccount {
         return false;
     }
 
+    private double readBalanceFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(BALANCE_FILE))) {
+            return Double.parseDouble(reader.readLine());
+        } catch (IOException | NumberFormatException e) {
+            // If file doesn't exist or is empty/invalid, return default balance
+            return 1000; // Default initial balance
+        }
+    }
 }
