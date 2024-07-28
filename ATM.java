3d7cde2c49bdb2ea87ee1ba9397fe6002102b1a1
Varad Kanekar <kanekarvarad@gmail.com>
2024-07-28 20:22:35 +0530

    method to display ATM menu

--- ATM.java
+++ ATM.java
@@ -6,4 +6,12 @@ class ATM {
     public ATM(BankAccount bankAccount) {
         account = bankAccount;
     }
+
+    public void displayMenu() {
+        System.out.println("ATM Menu:");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+    }
 }
