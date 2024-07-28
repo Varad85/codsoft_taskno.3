b4903192ea27fa182cfb29b7b158812bfc7f165b
Varad Kanekar <kanekarvarad@gmail.com>
2024-07-28 20:27:11 +0530

    method to run ATM interface for each option that user chooses

--- ATM.java
+++ ATM.java
@@ -14,4 +14,41 @@ class ATM {
         System.out.println("3. Withdraw");
         System.out.println("4. Exit");
     }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            displayMenu();
+            System.out.print("Select an option: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Your balance: Rs. " + account.getBalance());
+                    break;
+                case 2:
+                    System.out.print("Enter amount to deposit: ");
+                    double depositAmount = scanner.nextDouble();
+                    account.deposit(depositAmount);
+                    System.out.println("Deposit successful. Your balance: Rs. " + account.getBalance());
+                    break;
+                case 3:
+                    System.out.print("Enter amount to withdraw: ");
+                    double withdrawAmount = scanner.nextDouble();
+                    if (account.withdraw(withdrawAmount)) {
+                        System.out.println("Withdrawal successful. Your balance: Rs. " + account.getBalance());
+                    } else {
+                        System.out.println("Insufficient balance.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid option. Please select a valid option.");
+            }
+        }
+    }
 }
