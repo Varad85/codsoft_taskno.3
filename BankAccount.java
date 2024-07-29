265d3db33baf138979c069280ea357a2c0533308
Varad Kanekar <kanekarvarad@gmail.com>
2024-07-29 19:39:34 +0530

    final changes

--- BankAccount.java
+++ BankAccount.java
@@ -14,10 +14,10 @@ class BankAccount {
 
     public void deposit(double amount) {
         if(amount > 0){
-        balance += amount;
-        writeBalanceToFile();
-        System.out.println("Deposit successful");
-    }else{
+            balance += amount;
+            writeBalanceToFile();
+            System.out.println("Deposit successful");
+       }else{
         System.out.println("Deposit amount must be positive");
     }
 }
@@ -27,18 +27,25 @@ class BankAccount {
             System.out.println("Withdrawal amount must be positive.");
             return false;
         }
-        
+
         if (amount <= balance) {
             balance -= amount;
             writeBalanceToFile();
+            System.out.println("Withdrawal successful");
             return true;
+       }
+        if (amount > balance){
+            System.out.println("Insufficient balance");
+            return false;
         }
+
         return false;
-    }
+}
 
     private double readBalanceFromFile() {
         try (BufferedReader reader = new BufferedReader(new FileReader(BALANCE_FILE))) {
-            return Double.parseDouble(reader.readLine());
+            return Double.parseDouble(reader.readLine()); // Read and parse balance from file
+            
         } catch (IOException | NumberFormatException e) {
             // If file doesn't exist or is empty/invalid, return default balance
             return 1000; // Default initial balance
